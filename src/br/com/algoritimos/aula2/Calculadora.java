ebbf0dedb5ef7eaa80e804ab8fb9809da25932ae
Alex Novais <alexnovais@gmail.com>
2017-08-16 15:48:03 -0300

    Usando o IF para tomada de devisão

--- src/br/com/algoritimos/aula2/Calculadora.java
+++ src/br/com/algoritimos/aula2/Calculadora.java
@@ -1,31 +1,35 @@
 package br.com.algoritimos.aula2;
 
 public class Calculadora {
-	
+
 	String marca;
 	String modelo;
 	Integer tamanho;
 	Double peso;
-	
-	public int calcularSoma(int n1, int n2){
+
+	public int calcularSoma(int n1, int n2) {
 		int soma = n1 + n2;
 		return soma;
 	}
-	
-	public Double calcularSubtracao(Double n1, Double n2){
+
+	public Double calcularSubtracao(Double n1, Double n2) {
 		Double subtracao = n1 - n2;
 		return subtracao;
 	}
-	
-	public Double calcularMultiplicacao(Double n1, Double n2){
+
+	public Double calcularMultiplicacao(Double n1, Double n2) {
 		Double multiplicacao = n1 * n2;
 		return multiplicacao;
 	}
-	
-	public Double calcularDivisao(Double n1, Double n2){
-		Double divisao = n1 / n2;
+
+	public Double calcularDivisao(Double n1, Double n2) {
+		Double divisao = null;
+		if (n2 > 0) {
+			if (n1 > 0){
+			divisao = n1 / n2;
+			}
+		} 
 		return divisao;
 	}
 
-	
 }
